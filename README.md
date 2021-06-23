5b171263ea579361d7ab34fcaa9d290222cc9566
Eduardo Bart <edub4rt@gmail.com>
2021-06-23 14:52:24 -0300

    Refactor using a single source file

---
+++ README.md
@@ -0,0 +1,16 @@
+# NLDoc
+
+A Lua library used by generate documentation for the Nelua compiler.
+
+This library was created to generate documentation for the Nelua
+standard libraries.
+It can also be used to generate documentation for other projects,
+though you may have to personalize it for your needs.
+
+This library is theoretically easy to modify,
+it's just a single file, commented, and it does not
+depend on anything else than LPegRex.
+
+It contains the Nelua grammar definition inside.
+
+## Usage
---
+++ nelua-docs.lua
@@ -0,0 +1,165 @@
+local nldoc = require 'nldoc'
+
+-- Generate standard libraries documentation.
+local function gen_stdlib(neluadir)
+  local emitter = nldoc.Emitter.create()
+
+  emitter:add[[
+---
+layout: docs
+title: Libraries
+permalink: /libraries/
+categories: docs toc
+toc: true
+order: 4
+---
+
+This is a list of Nelua standard libraries.
+{: .lead}
+
+To use a library, use `require 'libraryname'`{:.language-nelua}.
+{: .callout.callout-info}
+
+]]
+
+  nldoc.generate_doc(emitter, neluadir..'/lib/builtins.nelua')
+  nldoc.generate_doc(emitter, neluadir..'/lib/arg.nelua')
+  nldoc.generate_doc(emitter, neluadir..'/lib/iterators.nelua')
+  nldoc.generate_doc(emitter, neluadir..'/lib/io.nelua')
+  nldoc.generate_doc(emitter, neluadir..'/lib/filestream.nelua')
+  nldoc.generate_doc(emitter, neluadir..'/lib/math.nelua')
+  nldoc.generate_doc(emitter, neluadir..'/lib/memory.nelua')
+  nldoc.generate_doc(emitter, neluadir..'/lib/os.nelua')
+  nldoc.generate_doc(emitter, neluadir..'/lib/span.nelua', {
+    include_names={spanT=true}
+  })
+  nldoc.generate_doc(emitter, neluadir..'/lib/string.nelua', {
+    include_names={string=true},
+    top_template = [[
+## $(name)
+
+$(text)
+
+### string
+
+```nelua
+global string = @record{
+  data: *[0]byte,
+  size: usize,
+}
+```
+
+The string record defined in the compiler sources.
+
+New strings always have the `data` buffer null terminated by default
+to have more comparability with C APIs.
+The `data` buffer is 0-indexed (unlike string APIs).
+]]
+  })
+  nldoc.generate_doc(emitter, neluadir..'/lib/stringbuilder.nelua', {
+    include_names={stringbuilderT=true}
+  })
+  nldoc.generate_doc(emitter, neluadir..'/lib/traits.nelua')
+  nldoc.generate_doc(emitter, neluadir..'/lib/utf8.nelua')
+  nldoc.generate_doc(emitter, neluadir..'/lib/coroutine.nelua')
+  nldoc.generate_doc(emitter, neluadir..'/lib/hash.nelua')
+  nldoc.generate_doc(emitter, neluadir..'/lib/vector.nelua', {
+    include_names={vectorT=true}
+  })
+  nldoc.generate_doc(emitter, neluadir..'/lib/sequence.nelua', {
+    include_names={sequenceT=true}
+  })
+  nldoc.generate_doc(emitter, neluadir..'/lib/list.nelua', {
+    include_names={listnodeT=true, listT=true}
+  })
+  nldoc.generate_doc(emitter, neluadir..'/lib/hashmap.nelua', {
+    include_names={hashnodeT=true, hashmapT=true}
+  })
+
+  nldoc.generate_doc(emitter, neluadir..'/lib/allocators/default.nelua', {
+    name='allocators.default',
+  })
+  nldoc.generate_doc(emitter, neluadir..'/lib/allocators/allocator.nelua', {
+    name='allocators.allocator',
+    include_names={Allocator=true}
+  })
+  nldoc.generate_doc(emitter, neluadir..'/lib/allocators/general.nelua', {
+    name='allocators.general',
+  })
+  nldoc.generate_doc(emitter, neluadir..'/lib/allocators/gc.nelua', {
+    name='allocators.gc',
+  })
+  nldoc.generate_doc(emitter, neluadir..'/lib/allocators/arena.nelua', {
+    name='allocators.arena',
+    include_names={ArenaAllocatorT=true}
+  })
+  nldoc.generate_doc(emitter, neluadir..'/lib/allocators/stack.nelua', {
+    name='allocators.stack',
+    include_names={StackAllocatorT=true}
+  })
+  nldoc.generate_doc(emitter, neluadir..'/lib/allocators/pool.nelua', {
+    name='allocators.pool',
+    include_names={PoolAllocatorT=true}
+  })
+  nldoc.generate_doc(emitter, neluadir..'/lib/allocators/heap.nelua', {
+    name='allocators.heap',
+    include_names={HeapAllocatorT=true}
+  })
+
+  emitter:add[[
+
+<a href="/clibraries/" class="btn btn-outline-primary btn-lg float-right">C Libraries >></a>
+]]
+
+  nldoc.write_file(neluadir..'/docs/pages/libraries.md', emitter:generate())
+end
+
+-- Generate C libraries documentation.
+local function gen_clib(neluadir)
+  local emitter = nldoc.Emitter.create()
+  emitter:add[[
+---
+layout: docs
+title: C libraries
+permalink: /clibraries/
+categories: docs toc
+toc: true
+order: 5
+---
+
+Nelua provides bindings for common C functions according to the C11 specification.
+This is a list of all imported C libraries.
+{: .lead}
+
+To use a C library, use `require 'C.stdlib'`{:.language-nelua} for example.
+{: .callout.callout-info}
+
+Nelua encourages you to use it's standard libraries instead of the C APIs,
+these are provided just as convenience for interoperating with C libraries.
+{:.alert.alert-info}
+
+]]
+  -- nldoc.generate_doc(emitter, neluadir..'/lib/C/init.nelua', {name='C',include_names={C=true}})
+  nldoc.generate_doc(emitter, neluadir..'/lib/C/arg.nelua', {name='C.arg',include_names={C=true}})
+  nldoc.generate_doc(emitter, neluadir..'/lib/C/ctype.nelua', {name='C.ctype',include_names={C=true}})
+  nldoc.generate_doc(emitter, neluadir..'/lib/C/errno.nelua', {name='C.errno',include_names={C=true}})
+  nldoc.generate_doc(emitter, neluadir..'/lib/C/locale.nelua', {name='C.locale',include_names={C=true}})
+  nldoc.generate_doc(emitter, neluadir..'/lib/C/math.nelua', {name='C.math',include_names={C=true}})
+  nldoc.generate_doc(emitter, neluadir..'/lib/C/signal.nelua', {name='C.signal',include_names={C=true}})
+  nldoc.generate_doc(emitter, neluadir..'/lib/C/stdarg.nelua', {name='C.stdarg',include_names={C=true}})
+  nldoc.generate_doc(emitter, neluadir..'/lib/C/stdio.nelua', {name='C.stdio',include_names={C=true}})
+  nldoc.generate_doc(emitter, neluadir..'/lib/C/stdlib.nelua', {name='C.stdlib',include_names={C=true}})
+  nldoc.generate_doc(emitter, neluadir..'/lib/C/string.nelua', {name='C.string',include_names={C=true}})
+  nldoc.generate_doc(emitter, neluadir..'/lib/C/time.nelua', {name='C.time',include_names={C=true}})
+
+  nldoc.write_file(neluadir..'/docs/pages/clibraries.md', emitter:generate())
+end
+
+if not arg[1] then
+  print 'Please pass the Nelua source directory as the first argument.'
+  os.exit(1)
+end
+
+local neluadir = arg[1]
+gen_stdlib(neluadir)
+gen_clib(neluadir)
